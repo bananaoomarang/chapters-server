0d3235409deb5700c81b315bad0c4e373de2e94f
Bananaoomarang <milomord@gmail.com>
2015-03-02 21:20:06 +0000

    Added uploading and loading for stories

--- resources/story/controller.js
+++ resources/story/controller.js
@@ -1,15 +1,39 @@
 'use strict';
 
+function trimExtension (filename) {
+  var split = filename.split('.');
+
+  split.pop();
+
+  return split.join('.');
+}
+
 module.exports = function (cfg) {
   var controller = {};
   var story      = require('./model')(cfg);
 
   controller.get = function (req, reply) {
-    reply('TODO');
+    story.get(req.params.title, function (err, text) {
+
+      if (err) return reply(err);
+
+      return reply(text);
+
+    });
   }
 
   controller.upload = function (req, reply) {
-    reply('TODO');
+    var file  = req.payload.file;
+    var title = trimExtension(file.hapi.filename);
+
+    story.save(req.payload.file, title, function (err) {
+
+      if (err) return reply(err);
+
+      return reply('Successfully saved story');
+
+    });
+
   }
 
   return controller;
--- resources/story/model.js
+++ resources/story/model.js
@@ -1,10 +1,51 @@
 'use strict';
 
+var fs    = require('fs');
 var db    = require('nano')('http://localhost:5984/_users');
-var debug = require('debug')('accounts');
+var debug = require('debug')('story');
 
 module.exports = function (cfg) {
   var model = {};
+
+  model.save = function (file, title, cb) {
+
+    var saveLocation = ['data', 'stories', title].join('/');
+    var write        = fs.createWriteStream(saveLocation, { encoding: 'utf-8' });
+
+    write.on('error', function (err) {
+
+      cb(err);
+
+    });
+
+    file.pipe(write);
+
+    return cb(null);
+
+  }
+
+  model.get = function (title, cb) {
+    
+    var location = ['data', 'stories', title].join('/');
+    var read     = fs.createReadStream(location, { encoding: 'utf-8' });
+    var text     = '';
+
+    read.on('error', function (err) {
+
+      cb(err)
+
+    })
+    .on('data', function (data) {
+
+      text += data.toString();
+
+    })
+    .on('end', function () {
+
+      cb(null, text);
+
+    });
+  };
 
   return model;
 
